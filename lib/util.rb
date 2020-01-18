79cc31729101de7b123680f56deffc7ca4e99b55
yakup <yakup.cetinkaya@gmail.com>
2020-01-18 14:55:34 -0300

    sat fix

--- lib/util.rb
+++ lib/util.rb
@@ -221,7 +221,7 @@ def parse_mapping
       data[row[1]] = {} unless data.has_key? row[1]
       data[row[1]][row[2]] = {} unless data[row[1]].has_key? row[2]
       meas = row[3]
-      meas = "#{row[9]} #{row[3]}" if row[9] and row[9].length > 0 and row[4] != "enum"
+      meas = "#{row[9]} #{row[3]}" if row[9] and row[9].length > 0 and not ["enum","binary","scale"].include?(row[4])
       #p ind,row if data[row[1]][row[2]].has_key? meas
       data[row[1]][row[2]][meas] = { "unit" => unit_sign(row[4]) }
       data[row[1]][row[2]][meas]["min"] = row[5] if row[5] and row[5].length > 0
@@ -235,7 +235,7 @@ def parse_mapping
       data_slug[row[1].slug] = {} unless data_slug.has_key? row[1].slug
       data_slug[row[1].slug][row[2].slug] = {} unless data_slug[row[1].slug].has_key? row[2].slug
       meas = row[3].slug
-      meas = "#{row[9].slug}-#{row[3].slug}" if row[9] and row[9].length > 0 and row[4] != "enum"
+      meas = "#{row[9].slug}-#{row[3].slug}" if row[9] and row[9].length > 0 and not ["enum","binary","scale"].include?(row[4])
       #p ind,row if data_slug[row[1].slug][row[2].slug].has_key? meas
       data_slug[row[1].slug][row[2].slug][meas] = { "unit" => row[4] }
       data_slug[row[1].slug][row[2].slug][meas]["min"] = row[5] if row[5] and row[5].length > 0
